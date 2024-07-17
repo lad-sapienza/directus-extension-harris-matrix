6f54b1f151ac4c735fa90af3e11d2a1ba8b7f714
Domenico Santoro <santo500@posteitaliane.it>
2024-07-17 22:44:22 +0200

    hmdj

--- src/engines/carafa.js
+++ src/engines/carafa.js
@@ -1,4 +1,16 @@
 var HmLog = require("../utils/hmlog.js");
+var HMDJ = require("../utils/hmdj.js");
+
+const hmdjMapping = {
+    "context_id": "id",
+    "context_label": "label",
+    "context_description": "description",
+    "properties": {
+        "key": "context_type",
+        "values": {}
+    },
+    "map_data": false
+};
 
 const engineVersion = function() {
 	return "Carafa engine *** v0.1 ***";
@@ -8,69 +20,53 @@ const engineVersion = function() {
 const prepareGraph = function(graphItems, contextProps) {
 	let items = graphItems;
 
-	//Loop effort to create a map
-	let validTargets = graphItems.reduce((obj, item) => {
-		return {
-			...obj,
-			[item["context_id"]]: true,
-		};
-	}, {});
-	
-	var nodesAttributes = {};
-	HmLog.hmLog('Vertex count: ' + items.length);
+    for (var pk in contextProps) {
+        hmdjMapping["properties"]["values"][pk] = contextProps[pk];
+    }
 
-	var nodes = [];
-	var arcs = [];
-	var props = {};
+    let hmdj = new HMDJ.HMDJ(hmdjMapping);
+    var nodesAttributes = {};
+    
+    HmLog.hmLog('Vertex count: ' + items.length);
 
 	for (var eidx in items) {
 		let node = items[eidx];
-		var nodeProps = [`shape="box"`];
-
-		if (node.context_type && contextProps[node.context_type] != null) {
-			HmLog.hmLog("Adding " + contextProps[node.context_type] + " as node props");
-			nodeProps = contextProps[node.context_type];
-		}
-
-		if (node.display_label) { nodeProps.push(`label="${node.display_label}"`); }
-		props[node["context_id"]] = nodeProps.join(",");
-		nodesAttributes[node["context_id"]] = { "id": node.id, "label": node.label, "text": node.description, "context_type": node.context_type }; //Could not figure out how to access images
-
-
-
-
-		nodes.push(`"${node["context_id"]}" [${nodeProps.join(",")}];`);
-
-		if (node["stratigraphy"]) {
-			for (var cix in node["stratigraphy"]) {
-				let child = node["stratigraphy"][cix];
-				var relation = "";
-
-				if (child["other_context"] == null) continue;
-				let otherContextId = child["other_context"]["context_id"];
-				if (validTargets[otherContextId] == null) continue;
-
-				if (child["relationship"]) {
-					if (['fills', 'covers', 'cuts', 'leans against'].includes(child["relationship"])) {
-						relation = `"${node["context_id"]}" -> "${otherContextId}";`;
-					} else if (['is filled by', 'is covered by', 'is cut by', 'carries'].includes(child["relationship"])) {
-						relation = `"${otherContextId}" -> "${node["context_id"]}";`;
-					} else if (['is the same as', 'is bound to'].includes(child["relationship"])) {
-						relation = `"${otherContextId}" -> "${node["context_id"]}" [style="dashed", color="blue", dir="none"];`;
-					} else {
-						HmLog.hmLog(`Not managed: ${child["relationship"]}`);
-						continue;
-					}
-				}
-
-				if (!arcs.includes(relation)) {
-					arcs.push(relation);
-				}
-			}
-		}
+		
+        nodesAttributes[node["context_id"]] = { "id": node.id, "label": node.label, "text": node.description, "context_type": node.context_type }; //Could not figure out how to access images
+        
+        hmdj.addContext(node, []);
+        
+//		if (node["stratigraphy"]) {
+//			for (var cix in node["stratigraphy"]) {
+//				let child = node["stratigraphy"][cix];
+//				var relation = "";
+//
+//				if (child["other_context"] == null) continue;
+//				let otherContextId = child["other_context"]["context_id"];
+//				if (validTargets[otherContextId] == null) continue;
+//
+//				if (child["relationship"]) {
+//					if (['fills', 'covers', 'cuts', 'leans against'].includes(child["relationship"])) {
+//						relation = `"${node["context_id"]}" -> "${otherContextId}";`;
+//					} else if (['is filled by', 'is covered by', 'is cut by', 'carries'].includes(child["relationship"])) {
+//						relation = `"${otherContextId}" -> "${node["context_id"]}";`;
+//					} else if (['is the same as', 'is bound to'].includes(child["relationship"])) {
+//						relation = `"${otherContextId}" -> "${node["context_id"]}" [style="dashed", color="blue", dir="none"];`;
+//					} else {
+//						HmLog.hmLog(`Not managed: ${child["relationship"]}`);
+//						continue;
+//					}
+//				}
+//
+//				if (!arcs.includes(relation)) {
+//					arcs.push(relation);
+//				}
+//			}
+//		}
 	}
 
-	return {"graph": nodes.join("\n") + "\n" + arcs.join("\n"), "attributes": nodesAttributes};
+	//return {"graph": nodes.join("\n") + "\n" + arcs.join("\n"), "attributes": nodesAttributes};
+    return {"graph": "", "attributes": nodesAttributes};
 }
 
 module.exports = { engineVersion, prepareGraph }
---
+++ src/utils/hmdj.js
@@ -0,0 +1,56 @@
+const HMDJ = function(mapping) {
+    this.id = "hmdj_" + new Date().getTime() + "_" + Math.floor(Math.random() * 100000000);
+    this.mapping = mapping;
+    
+    this.nodes = {};
+    
+    this.describe = function() {
+        console.log("HMDJ: " + this.id);
+        console.log("HMDJ: " + this.stringify());
+    }
+    
+    this.stringify = function(as_array) {
+        var o2s = this.nodes;
+        if (as_array == true) {
+            o2s = Object.keys(this.nodes).map(key => this.nodes[key]);
+        }
+        return JSON.stringify(o2s, null, 4);
+    }
+    
+    this.addContext = function(context, relationships) {
+        var nodeIdKey = "id";
+        if (mapping && "context_id" in mapping) { nodeIdKey = mapping["context_id"] }
+        
+        var rels = [];
+        if(relationships) { rels = relationships; }
+        
+        if (nodeIdKey in context) {
+            let nodeId = context[nodeIdKey];
+            this.nodes[nodeId] = {
+                "id": nodeId,
+                "relationships": rels
+            }
+            if (mapping) {
+                if ("map_data" in mapping && mapping["map_data"] == true) { this.nodes[nodeId]["data"] = context; }
+                if ("properties" in mapping) {
+                    let propKey = mapping["properties"]["key"];
+                    if(propKey in context) {
+                        let propValue = context[propKey];
+                        if (propValue in mapping["properties"]["values"]) {
+                            this.nodes[nodeId]["properties"] = mapping["properties"]["values"][propValue];
+                        }
+                    }
+                }
+                var contextLabelKey = "label";
+                if ("context_label" in mapping) { contextLabelKey = mapping["context_label"]; }
+                if(context[contextLabelKey]) { this.nodes[nodeId]["label"] = context[contextLabelKey]; }
+                var contextDescKey = "description";
+                if ("context_description" in mapping) { contextDescKey = mapping["context_description"]; }
+                if (context[contextDescKey]) { nodeIdKey = this.nodes[nodeId]["description"] = context[contextDescKey]; }
+            }
+        } else {
+            console.error("Could not find context id");
+        }
+    }
+}
+module.exports = { HMDJ }
